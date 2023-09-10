e40741025e010dfce5962088f4d8dadea7c1a215
picturesonpictures <118248359+picturesonpictures@users.noreply.github.com>
2023-09-09 23:38:55 -0500

    Add files via upload
    adding new things

---
+++ ConditioningPoP.py
@@ -0,0 +1,86 @@
+class ConditioningMultiplierPoP:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"conditioning": ("CONDITIONING", ), "multiplier": ("FLOAT", {"default": 1.0, "min": -1, "max": 3.0})}}
+
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "multiply_conditioning_strength"
+
+    def multiply_conditioning_strength(self, conditioning, multiplier):
+        # Validate the input types for 'conditioning' and 'multiplier'
+        if not isinstance(conditioning, list) or not isinstance(multiplier, float):
+            raise ValueError("Invalid input types")
+
+        #Initialize a new list to store the modified conditioning objects
+        new_conditioning = []
+
+        # Iterate through each element in the 'conditioning' list
+        for index, (tensor, attributes) in enumerate(conditioning):
+            # Multiply the tensor by the given multiplier
+            new_tensor = tensor.clone()
+            new_attributes = attributes.copy()
+
+            # Multiply the new tensor by the given multiplier
+            new_tensor *= multiplier
+
+            # If 'pooled_output' exists, scale it by the multiplier
+            if "pooled_output" in attributes:
+                new_pooled_output = attributes["pooled_output"].clone()
+                new_pooled_output *= multiplier
+                new_attributes["pooled_output"] = new_pooled_output
+
+            # Add the modified tensor and attributes to the new_conditioning list
+            new_conditioning.append([new_tensor, new_attributes])
+
+
+        # Return the modified 'conditioning' object
+        return (new_conditioning, )  # NOTE: Returning new_conditioning here
+
+class ConditioningNormalizerPoP:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"conditioning": ("CONDITIONING", )}}
+
+    RETURN_TYPES = ("CONDITIONING",)
+    FUNCTION = "normalize_conditioning"
+
+    def normalize_conditioning(self, conditioning):
+        # Validate the input type for 'conditioning'
+        if not isinstance(conditioning, list):
+            raise ValueError("Invalid input type")
+
+        # Initialize a new list to store the modified conditioning objects
+        new_conditioning = []
+
+        # Iterate through the 'conditioning' list
+        for index, (tensor, attributes) in enumerate(conditioning): #Q what is this doing? A iterating through the conditioning list
+            # Create new objects to store modified tensor and attributes
+            new_tensor = tensor.clone()
+            new_attributes = attributes.copy()
+
+            # Normalize the new tensor to have zero mean and unit variance
+            new_tensor -= new_tensor.mean()
+            new_tensor /= new_tensor.std()
+
+            # If 'pooled_output' exists, normalize it
+            if "pooled_output" in attributes:
+                new_pooled_output = attributes["pooled_output"].clone()
+                new_pooled_output -= new_pooled_output.mean()
+                new_pooled_output /= new_pooled_output.std()
+                new_attributes["pooled_output"] = new_pooled_output
+
+            # Add the modified tensor and attributes to the new_conditioning list
+            new_conditioning.append([new_tensor, new_attributes])
+
+        # Return the modified 'conditioning' object
+        return (new_conditioning, )  # NOTE: Returning new_conditioning here
+
+#create node class mappings and node display name mappings
+NODE_CLASS_MAPPINGS = {
+    "ConditioningMultiplierPoP": ConditioningMultiplierPoP,
+    "ConditioningNormalizerPoP": ConditioningNormalizerPoP
+} 
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ConditioningMultiplierPoP": "Conditioning Multiplier PoP",
+    "ConditioningNormalizerPoP": "Conditioning Normalizer PoP"
+}
--- __init__.py
+++ __init__.py
@@ -1,13 +1,20 @@
+
 from .AnyAspectRatio import NODE_CLASS_MAPPINGS as AnyAspectRatio_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as AnyAspectRatio_DISPLAY_NAME_MAPPINGS
 from .LoraStackLoader_PoP import NODE_CLASS_MAPPINGS as LoraStackLoader_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as LoraStackLoader_PoP_DISPLAY_NAME_MAPPINGS
 from .LoraStackLoader10_PoP import NODE_CLASS_MAPPINGS as LoraStackLoader10_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as LoraStackLoader10_PoP_DISPLAY_NAME_MAPPINGS
 from .AdaptiveCannyDetector_PoP import NODE_CLASS_MAPPINGS as AdaptiveCannyDetector_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as AdaptiveCannyDetector_PoP_DISPLAY_NAME_MAPPINGS
+from .VAEEncodeDecodeLoader import NODE_CLASS_MAPPINGS as VAEEncoderPoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as VAEEncoderPoP_DISPLAY_NAME_MAPPINGS
+from .VAEEncodeDecodeLoader import NODE_CLASS_MAPPINGS as VAEDecoderPoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as VAEDecoderPoP_DISPLAY_NAME_MAPPINGS
+from .ConditioningPoP import NODE_CLASS_MAPPINGS as ConditioningMultiplierPoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as ConditioningMultiplierPoP_DISPLAY_NAME_MAPPINGS   
 
 NODE_CLASS_MAPPINGS = {
     **AnyAspectRatio_MAPPINGS,
     **LoraStackLoader_PoP_MAPPINGS,
     **LoraStackLoader10_PoP_MAPPINGS,
     **AdaptiveCannyDetector_PoP_MAPPINGS,
+    **VAEEncoderPoP_MAPPINGS,
+    **VAEDecoderPoP_MAPPINGS,
+    **ConditioningMultiplierPoP_MAPPINGS,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -15,6 +22,10 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     **LoraStackLoader_PoP_DISPLAY_NAME_MAPPINGS,
     **LoraStackLoader10_PoP_DISPLAY_NAME_MAPPINGS,
     **AdaptiveCannyDetector_PoP_DISPLAY_NAME_MAPPINGS,
+    **VAEEncoderPoP_DISPLAY_NAME_MAPPINGS,
+    **VAEDecoderPoP_DISPLAY_NAME_MAPPINGS,
+    **ConditioningMultiplierPoP_DISPLAY_NAME_MAPPINGS,
+    
 }
 
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+
