ca9597dc5e97660a20464d6e735415dc003ac6aa
picturesonpictures <118248359+picturesonpictures@users.noreply.github.com>
2024-07-05 02:24:28 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ from .AdaptiveCannyDetector_PoP import NODE_CLASS_MAPPINGS as AdaptiveCannyDetec
 from .LoadImageResizer_PoP import NODE_CLASS_MAPPINGS as LoadImageResizer_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as LoadImageResizer_PoP_DISPLAY_NAME_MAPPINGS
 from .Conditioning_PoP import NODE_CLASS_MAPPINGS as Conditioning_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as Conditioning_PoP_DISPLAY_NAME_MAPPINGS
 from .VAEEncodeDecodeLoader_PoP import NODE_CLASS_MAPPINGS as VAEEncodeDecodeLoader_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as VAEEncodeDecodeLoader_PoP_DISPLAY_NAME_MAPPINGS 
-from .openAI_PoP import NODE_CLASS_MAPPINGS as openAI_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as openAI_PoP_DISPLAY_NAME_MAPPINGS
 from .EfficientAttentionNode_PoP import NODE_CLASS_MAPPINGS as EfficientAttention_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as EfficientAttention_PoP_DISPLAY_NAME_MAPPINGS
+from .openAI_PoP import NODE_CLASS_MAPPINGS as openAI_PoP_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS as openAI_PoP_DISPLAY_NAME_MAPPINGS
 
 NODE_CLASS_MAPPINGS = {
     **AnyAspectRatio_MAPPINGS,
@@ -14,8 +14,8 @@ NODE_CLASS_MAPPINGS = {
     **LoadImageResizer_PoP_MAPPINGS,
     **Conditioning_PoP_MAPPINGS,
     **VAEEncodeDecodeLoader_PoP_MAPPINGS,
-    **openAI_PoP_MAPPINGS,
-    **EfficientAttention_PoP_MAPPINGS  # Add this line
+    **EfficientAttention_PoP_MAPPINGS,  
+    **openAI_PoP_MAPPINGS
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -25,8 +25,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     **LoadImageResizer_PoP_DISPLAY_NAME_MAPPINGS,
     **Conditioning_PoP_DISPLAY_NAME_MAPPINGS,
     **VAEEncodeDecodeLoader_PoP_DISPLAY_NAME_MAPPINGS,
+    **EfficientAttention_PoP_DISPLAY_NAME_MAPPINGS,
     **openAI_PoP_DISPLAY_NAME_MAPPINGS,
-    **EfficientAttention_PoP_DISPLAY_NAME_MAPPINGS  # Add this line
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
